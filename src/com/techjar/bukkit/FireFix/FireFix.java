b210f3be4902c416b5ddb82de53eaf8ca8c21bff
Techjar <tecknojar@gmail.com>
2011-04-23 17:47:26 -0400

    Added configuration, it should work correctly.

--- src/com/techjar/bukkit/FireFix/FireFix.java
+++ src/com/techjar/bukkit/FireFix/FireFix.java
@@ -12,6 +12,7 @@
 
 package com.techjar.bukkit.FireFix;
 
+import java.io.File;
 import java.util.logging.Logger;
 import java.util.logging.Level;
 //import org.bukkit.plugin.PluginDescriptionFile;
@@ -19,19 +20,22 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.Event.Priority;
 import org.bukkit.event.Event.Type;
+import org.bukkit.util.config.Configuration;
 
 public class FireFix extends JavaPlugin {
     static final Logger logger = Logger.getLogger("Minecraft.FireFix");
     private final LBlock blockListener = new LBlock(this);
     private final LPlayer playerListener = new LPlayer(this);
+    private Configuration config;
     
     @Override
     public void onEnable() {
         try {
+            loadConfig();
             PluginManager pm = getServer().getPluginManager();
-            pm.registerEvent(Type.BLOCK_IGNITE, blockListener, Priority.Highest, this);
+            if(config.getBoolean("firefix.rainfire", true)){pm.registerEvent(Type.BLOCK_IGNITE, blockListener, Priority.Highest, this);}
             //pm.registerEvent(Type.PLAYER_ANIMATION, playerListener, Priority.Highest, this);
-            pm.registerEvent(Type.PLAYER_INTERACT, playerListener, Priority.Highest, this);
+            if(config.getBoolean("firefix.punchfire", true)){pm.registerEvent(Type.PLAYER_INTERACT, playerListener, Priority.Highest, this);}
             logger.log(Level.INFO, getDescription().getName() + " version " + getDescription().getVersion() + " enabled.");
         }
         catch (Throwable e) {
@@ -43,6 +47,21 @@ public class FireFix extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        logger.log(Level.INFO, getDescription().getName() + " version " + getDescription().getVersion() + " is disabled.");
+        logger.log(Level.INFO, getDescription().getName() + " version " + getDescription().getVersion() + " disabled.");
+    }
+
+    private void loadConfig() {
+        File dataDirectory = new File("plugins" + File.separator + "FireFix" + File.separator);
+        dataDirectory.mkdirs();
+        File file = new File("plugins" + File.separator + "FireFix", "config.yml");
+        config = new Configuration(file);
+        config.load();
+        if(!file.exists()) {
+            logger.log(Level.INFO, getDescription().getName() + " config.yml not found, creating default configuration...");
+            config.setProperty("firefix.punchfire", true);
+            config.setProperty("firefix.rainfire", true);
+            config.save();
+        }
+        logger.log(Level.INFO, getDescription().getName() + " configuration loaded.");
     }
 }
